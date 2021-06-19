ee1be831ea51cec4235bcd21182a50aaf6ada1ca
gbgamzy <bhatejagautam1@gmail.com>
2021-06-19 23:15:00 +0530

    Phone Auth added

--- ajuba_merchant/lib/main.dart
+++ ajuba_merchant/lib/main.dart
@@ -1,24 +1,31 @@
+// ignore: import_of_legacy_library_into_null_safe
 import 'package:ajuba_merchant/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'login.dart';
 import './utils/routes.dart';
 
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+
+
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.dark,
       theme: ThemeData(primarySwatch: Colors.indigo,
-          //fontFamily: GoogleFonts.lato().fontFamily
+        //fontFamily: GoogleFonts.lato().fontFamily
 
       ),
 
@@ -26,7 +33,8 @@ class MyApp extends StatelessWidget {
       initialRoute: Routes.login,
       routes: {
         '/': (context)=> LoginPage(),
-        Routes.login: (context) => LoginPage()
+        Routes.login: (context) => LoginPage(),
+        Routes.home:(context) => HomePage()
 
       },
     );
